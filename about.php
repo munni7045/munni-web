c456988d182391c1f0fa46fcc3d4317da988403f
munni7045 <92568828+munni7045@users.noreply.github.com>
2021-10-15 12:55:24 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 4 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    data-target="navbarNav" aria-controls="navbarNav" 
+    aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="IMMUNNI/2.JPG" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="IMMUNNI/1.JPG" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="IMMUNNI/3.JPG" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<Body>
+<Html>
---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  * {margin: 0; padding: 0; box-sizing: border-box; }
+  .carousel-inner img {
+    width: 100%;
+    height: 90%;
+  {
+  .aboutimg
+  width: 100%;
+  height: 150px!important;
+  }
+  </style>
+</head>
+<body>
+<div class="container-fluid">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">mila carpenter</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">about</a>
+      </li>    
+    </ul>
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+  </div>  
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="IMMUNNI/m11.jpg" alt="Los Angeles" width="110" height="100">
+      <div class="carousel-caption">
+        <h3>malad</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="IMMUNNI/m11.jpg" alt="Chicago" width="110" height="100">
+      <div class="carousel-caption">
+        <h3>andheri</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="IMMUNNI/m11.jpg" alt="New York" width="110" height="100">
+      <div class="carousel-caption">
+        <h3>khandiwali</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+ <section class="row-5">
+  <div class="py-0">
+    <h2 class="text-center">about us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="IMMUNNI/m6.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display">i am son of shatyamurti.</h2>
+      <p class="py-5">
+        A paragraph is a self-contained  
+        A paragraph is a self-contained  
+        discourse in writing dealing wi 
+         particular point or idea. A par
+         of one or more sentences. Thoug
+         the syntax of any language, par
+         an expected part of formal
+         discourse in writing dealing wi 
+         particular point or idea. A par
+         of one or more sentences. Thoug
+         the syntax of any language, par
+         an expected part of formal 
+         A paragraph is a self-contained  
+        A paragraph is a self-contained  
+        discourse in writing dealing wi 
+         particular point or idea. A par
+         of one or more sentences. Thoug
+         the syntax of any language, par
+         an expected part of formal
+         discourse in writing dealing wi 
+         particular point or idea. A par
+         of one or more sentences. Thoug
+         the syntax of any language, par
+         
+        </p>
+         <a href="about.php" class="btn btn-success"> check more </a>
+    </div>
+  </div>
+</div>  
+  </section>
+                 <section class="row-5">
+  <div class="py-0">
+    <h2 class="text-center">our services</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="IMMUNNI/m10.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">John Doe</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="IMMUNNI/m10.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">John Doe</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="IMMUNNI/m10.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">John Doe</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="my-0">
+  <div class="py-0">
+    <h2 class="text-center">our gallery</h2>
+</div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+      <img src="IMMUNNI/m9.jpg" class="img-fluid pb-4">
+</div>
+</div>
+</div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">contact us</h2>
+</div>
+
+<div class="w-50 m-auto">
+  <form action="userinfo.php" method="POST">
+    <div class="form-group">
+      <label>username</label>
+       <input type="text" name="user" autocomplete="off"
+       class="form-control">  
+</div>
+<div class="form-group">
+      <label>email</label>
+       <input type="text" name="email" autocomplete="off"
+       class="form-control">  
+</div>
+<div class="form-group">
+      <label>mobile</label>
+       <input type="text" name="mobile" autocomplete="off"
+       class="form-control">  
+</div>
+
+<div class="form-group">
+      <label>comments</label>
+       <textarea class="form-control" name="comment"></textarea>  
+</div>
+
+<button type="submit" class="btn btn-success">submit</button>
+</form>
+</div>
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">
+    @munnisharma mila carpenter</p>
+</footer>
+</body>
+</html>
+
